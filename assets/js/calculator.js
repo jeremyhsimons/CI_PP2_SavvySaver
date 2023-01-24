9d0e2a712c1984350ad218da70a62699842f6192
jeremyhsimons <jeremysimons15@gmail.com>
2023-01-24 16:23:36 +0000

    Add event listener to pre-populate calc form when DOM loads.

---
+++ assets/js/calculator.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function fillForm() {
+    let fields = document.getElementsByTagName('input');
+    //fields[0].value = 1000;
+    let placeholders = [1500, 200, 50, 100, 150, 300, 1, 12, 100]
+
+    for (let i = 0; i <= placeholders.length; i++) {
+        fields[i].value = placeholders[i];
+    }
+
+    
+})
+
+
+
+/**
+ * A function to get all the user's inputs from the form and store them
+ * in an array called inputs.
+ * This function is called by form submission event.
+ */
+function getInputs() {
+
+}
+
+/**
+ * A function to calculate the user's projected savings.
+ * This function is called after the the inputs array is filled.
+ */
+function calcSaving() {
+
+}
+
+/**
+ * A function to check whether the user's projected saving
+ * is greater than or equal to the savings goal.
+ */
+function checkSaving() {
+
+}
+
+/**
+ * A function that loops through the user's inputs and calculates
+ * the revised inputs needed for the user to meet their
+ * savings goal.
+ * 
+ * This function is only called if the checkSaving function finds
+ * that the user's savings are below the goal.
+ */
+function calcSavingChanges() {
+
+}
+
+/**
+ * A function that checks each of the user's inputs against
+ * pre-determined recommended figures and outputs HMTL accordingly.
+ * 
+ * E.g. If the user's rent > 25% of monthly income
+ * a warning will appear suggesting that they minimise this cost.
+ * 
+ * This function is called after the Inputs array is filled.
+ */
+function checkRecommemndations() {
+
+}
