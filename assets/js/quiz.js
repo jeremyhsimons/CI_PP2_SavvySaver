bcf7128fcdb3085637cb3f2ab0fc4b2ec5c3ab23
jeremyhsimons <jeremysimons15@gmail.com>
2023-02-01 13:58:11 +0000

    Add questions/answers data to quiz.js file.

---
+++ assets/js/quiz.js
@@ -0,0 +1,54 @@
+//Questions and answers data for the quiz.
+
+let questions = [
+
+    //Question 1
+    {
+        question: 'According to financial guru Dave Ramsay, what is the maximum proportion of your income that should go towards your rent or mortgage costs?',
+        a: '30%',
+        b: '10%',
+        c: '50%',
+        d: '25%',
+        answer: 'd',
+    },
+
+    //Question 2
+    {
+        question: 'What was the British Gas estimated monthly gas bill for a 5-bed household in the UK for 2022?',
+        a: '£90',
+        b: '£155',
+        c: '£124',
+        d: '£189',
+        answer: 'b',
+    },
+
+    //Question 3
+    {
+        question: 'In your budget, which expense should usually be the largest?',
+        a: 'Housing',
+        b: 'Car payments',
+        c: 'Holidays',
+        d: 'Gas and electricity bills',
+        answer: 'a',
+    },
+
+    //Question 4
+    {
+        question: 'What is a good rule of thumb if you want to budget giving to charity?',
+        a: '5% of monthly income',
+        b: '6% of monthly income',
+        c: '8% of monthly income',
+        d: '10% of monthly income',
+        answer: 'd',
+    },
+
+    //Question 5
+    {
+        question: 'If you are not meeting your savings target, what can you do to sustainably improve your finances?',
+        a: 'Take on another part-time job',
+        b: 'Start day trading on the stock market',
+        c: 'Cut monthly expenses',
+        d: 'Take out a loan',
+        answer: 'c',
+    },
+]
