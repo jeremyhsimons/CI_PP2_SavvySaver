7c623a26b2ed102f0afbe6fd81baef42834de655
jeremyhsimons <jeremysimons15@gmail.com>
2023-02-01 18:03:31 +0000

    Add code from emailjs to set up contact form. Alter html of contact form to fit emailjs code.

---
+++ assets/js/contact_form.js
@@ -0,0 +1,22 @@
+//Code from EmailJS. Link to original in readme. 
+
+const btn = document.getElementById('contact-submit');
+
+document.getElementById('contact-form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_5frzz6d';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
